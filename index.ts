91fb4ccb947c31498f372ed697203a1a47962c12
rainy-25Ghz <1748420480@qq.com>
2021-12-07 23:06:28 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@
 import './style.css';
 import { Chart } from '@antv/g2';
 import { animate } from 'popmotion/lib/animations';
-const colors=
-const data = [
+const colors = ['#F8B195', '#F67280', '#C06C84', '#6C5B7B', '#355C7D'];
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+let data = [
   { year: '1951 年', sales: 38 },
   { year: '1952 年', sales: 52 },
   { year: '1956 年', sales: 61 },
@@ -27,6 +28,20 @@ chart.tooltip({
 });
 chart.interaction('active-region');
 
-chart.interval().position('year*sales');
-
-chart.render();
+chart.interval().position('year*sales').color('year');
+for (let j = 0; j < 1000; j++)
+  for (let i = 0; i < data.length; i++)
+    animate({
+      from: Math.ceil(Math.random() * 100) + 10,
+      to: -Math.ceil(Math.random() * 100) + 110,
+      onUpdate: (latest) => {
+        data[i] = { year: data[i].year, sales: latest };
+        chart.data(data);
+        chart.tooltip({
+          showMarkers: false,
+        });
+        chart.interaction('active-region');
+        chart.interval().position('year*sales').color('year');
+        chart.render();
+      },
+    });
